cd4ab4fbdad28f55498c633e0989005d0ce698c0
CorScorpii <wwyyuuuu@gmail.com>
2020-08-30 17:06:21 +0800

    feat: update week21 homework

---
+++ week21/publish/publish-tool/publish.js
@@ -0,0 +1,40 @@
+const http = require('http')
+const querystring = require('querystring')
+const fs = require('fs')
+const archiver = require('archiver')
+const child_process = require('child_process')
+
+let packname = './package'
+
+const options = {
+  host: 'localhost',
+  port: 8088,
+  path: '/?filename=' + 'package.zip',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/octet-stream',
+  },
+}
+
+const req = http.request(options, (res) => {
+  console.log(`STATUS: ${res.statusCode}`)
+  console.log(`HEADERS: ${JSON.stringify(res.headers)}`)
+})
+req.on('error', (e) => {
+  console.error(`problem with request: ${e.message}`)
+})
+
+var archive = archiver('zip', {
+  zlib: { level: 9 }, //Sets the compression level.
+})
+
+archive.directory(packname, false)
+
+archive.finalize()
+
+archive.pipe(req)
+
+archive.on('end', () => {
+  req.end()
+  console.log('publish success!!')
+})
